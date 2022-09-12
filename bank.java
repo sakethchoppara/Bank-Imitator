d39116bb4a6b8e5e7f02fc5b9d90397ab82c4e5e
CodeRider000 <112244064+CodeRider000@users.noreply.github.com>
2022-09-12 22:49:44 +0530

    Update bank.java
    added comments and few new lines

--- bank.java
+++ bank.java
@@ -1,27 +1,38 @@
+//created a class named SavingsAccount to calculate the users monthly intreset and balance
 class SavingsAccount{
-    double intrestRate = 4.0;
+    private double intrestRate = 0.0,mi;
     private double balance;
     SavingsAccount(double b){
         balance = b;
     }
+    //this method sets the intreset rate
     void setInstrestRate(double newInstrest){
-        intrestRate = newInstrest;
+        this.intrestRate = newInstrest;
     }
+    //this method is to get monthly intreset
     double getMonthlyIntrest(){
-        double mi = (intrestRate*balance)/12;
-        return mi;
+        this.mi = (this.intrestRate*this.balance)/12;
+        return this.mi;
+    }
+    //this method is to get balance
+    double getBalance(){
+        this.balance = this.balance+mi;
+        return this.balance;
     }
 }
 
 public class bank {
     public static void main(String[] args) {
+    //creating two objects with saver1, saver2 with 2000 and 3000 as bank balance as respectivly
     SavingsAccount saver1 = new SavingsAccount(2000);
     SavingsAccount saver2 = new SavingsAccount(3000);
-    System.out.println("Monthly Instrest of saver1 : "+ saver1.getMonthlyIntrest());
-    System.out.println("Monthly Intrest of saver 2 : "+ saver2.getMonthlyIntrest());
-    saver1.setInstrestRate(4);
-    saver2.setInstrestRate(5);
-    System.out.println("Monthly intrest of saver 1 after change : "+ saver1.getMonthlyIntrest());
-    System.out.println("monthly intrest of saver 2 after change : "+ saver2.getMonthlyIntrest());
+    saver1.setInstrestRate(4.0);
+    saver2.setInstrestRate(5.0);
+    //printing the final verdict
+    System.out.println("Saver1 monthly intreset : "+saver1.getMonthlyIntrest());
+    System.out.println("saver1 balance : "+saver1.getBalance());
+    System.out.println("saver2 monthly intreset : "+saver2.getMonthlyIntrest());
+    System.out.println("saver2 balance : "+saver2.getBalance());
+
 }
 }
